7762c69405139cbd27c91051d7815194dff0900e
Eden <azurehound.landline090@slmail.me>
2026-07-17 16:20:54 +0530

    feat(nixos): reduce svr closure

--- modules/nixos/hardware/media/video.nix
+++ modules/nixos/hardware/media/video.nix
@@ -1,3 +1,18 @@
 {
-  hardware.graphics.enable = true;
+  self,
+  pkgs,
+  config,
+  ...
+}:
+
+let
+  inherit (self.lib) isx86Linux;
+in
+
+{
+  hardware.graphics = {
+    enable = true;
+
+    enable32Bit = config.pixel.profiles.graphical.enable && isx86Linux pkgs;
+  };
 }
