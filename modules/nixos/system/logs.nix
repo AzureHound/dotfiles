33eb4d37a3cae6d8d9bf7fcd4c38eef080d5bce6
Eden <azurehound.landline090@slmail.me>
2026-09-12 23:30:46 +0530

    Merge branch 'journald' into update-flake-inputs

--- modules/nixos/system/logs.nix
+++ modules/nixos/system/logs.nix
@@ -7,10 +7,10 @@ in
 {
   config = mkIf (!config.pixel.profiles.server.enable) {
     # https://wiki.archlinux.org/title/Systemd/Journal#Persistent_journals
-    services.journald.extraConfig = ''
-      SystemMaxUse=100M
-      RuntimeMaxUse=50M
-      SystemMaxFileSize=50M
-    '';
+    services.journald.settings.Journal = {
+      SystemMaxUse = "100M";
+      RuntimeMaxUse = "50M";
+      SystemMaxFileSize = "50M";
+    };
   };
 }
