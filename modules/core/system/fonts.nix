2887a2126dcf75bbd554aef1c1e9f5ecc747294c
Eden <azurehound.landline090@slmail.me>
2026-09-15 14:40:53 +0530

    core/system/fonts: Added darwin fonts

--- modules/core/system/fonts.nix
+++ modules/core/system/fonts.nix
@@ -10,6 +10,12 @@
 let
   inherit (lib.lists) optionals;
   inherit (lib.modules) mkIf;
+
+  fontPath = "${self}/config/home/.local/share/fonts";
+  install = font: ''
+    mkdir -p $out/share/fonts/truetype
+    cp $src $out/share/fonts/truetype/${font}
+  '';
 in
 
 {
@@ -36,16 +42,6 @@ in
         twemoji-color-font
 
         maple-mono.truetype
-
-        (stdenvNoCC.mkDerivation {
-          name = "josefin-sans";
-          src = "${self}/config/home/.local/share/fonts/JosefinSans";
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/share/fonts/truetype
-            cp -r $src/* $out/share/fonts/truetype/
-          '';
-        })
       ]
 
       ++ (with pkgs.nerd-fonts; [
@@ -53,15 +49,38 @@ in
         symbols-only
       ])
 
-      ++ optionals (_class == "nixos") [
+      ++ optionals (_class == "darwin") [
         (stdenvNoCC.mkDerivation {
-          name = "pixelon";
-          src = "${self}/config/home/.local/share/fonts/pixelon.regular.ttf";
+          name = "josefin-sans";
+          src = "${fontPath}/JosefinSans";
           dontUnpack = true;
           installPhase = ''
             mkdir -p $out/share/fonts/truetype
-            cp $src $out/share/fonts/truetype/pixelon.regular.ttf
+            cp -r $src/* $out/share/fonts/truetype/
           '';
+        })
+
+        (stdenvNoCC.mkDerivation {
+          name = "fluent-system-icons";
+          src = "${fontPath}/fluent-system-icons.ttf";
+          dontUnpack = true;
+          installPhase = install "fluent-system-icons.ttf";
+        })
+
+        (stdenvNoCC.mkDerivation {
+          name = "tabler-icons";
+          src = "${fontPath}/tabler-icons.ttf";
+          dontUnpack = true;
+          installPhase = install "tabler-icons.ttf";
+        })
+      ]
+
+      ++ optionals (_class == "nixos") [
+        (stdenvNoCC.mkDerivation {
+          name = "pixelon";
+          src = "${fontPath}/pixelon.regular.ttf";
+          dontUnpack = true;
+          installPhase = install "pixelon.regular.ttf";
         })
       ];
   };
